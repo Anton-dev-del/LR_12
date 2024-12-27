d2315b4329fcfa739d7c081e27c06f3de325dc33
Anton-dev-del <anton06gar@gmail.com>
2024-12-27 11:50:10 +0300

    Add files via upload

---
+++ ЛР 12/4.pas
@@ -0,0 +1,37 @@
+﻿var
+  inp, tem: text;
+  line: string;
+  K, cur: integer;
+begin
+  writeln('Введите номер строки K для вставки пустой строки:');
+  readln(K);
+
+  assign(inp, 'cat.txt');
+  reset(inp);
+
+  // Открытие временного файла для записи
+  assign(tem, 'temp.txt');
+  rewrite(tem);
+
+  cur := 1;
+
+  while not eof(inp) do
+  begin
+    readln(inp, line);
+
+    if cur = K then
+      writeln(tem, '');
+
+    writeln(tem, line);
+    
+    cur := cur + 1;
+  end;
+  close(inp);
+  close(tem);
+
+  // Замена ориг файла временным
+  erase(inp);
+  rename(tem, 'cat.txt');
+
+  writeln('Пустая строка успешно вставлена перед строкой номер ', K, '.');
+end.
---
+++ ЛР 12/7.pas
@@ -0,0 +1,52 @@
+﻿program Zad7;
+
+var
+    n, sum, p: Int64;
+    isPrime: array[1..1000000] of Boolean;
+
+procedure Si(limit: LongInt);
+var
+    i, j: LongInt;
+begin
+    for i := 2 to limit do
+        isPrime[i] := True;
+
+    for i := 2 to trunc(Sqrt(limit)) do
+        if isPrime[i] then
+            for j := i * i to limit do
+                if j mod i = 0 then
+                    isPrime[j] := False;
+end;
+begin
+    Assign(input, 'z3.in.txt');
+    Reset(input);
+    
+    if not Eof(input) then begin
+        ReadLn(n);
+    end else begin
+        WriteLn('Ошибка: файл z3.in пуст или не существует');
+        Exit;
+    end;
+    Close(input);
+
+    sum := 0;
+
+    Si(1000000);
+
+    // Поиск чисел p^4 и их суммирование
+    p := 2;
+    while p <= 1000000 do
+    begin
+        if isPrime[p] then
+        begin
+            if (p * p * p * p) <= n then
+                sum := sum + (p * p * p * p);
+        end;
+        Inc(p);
+    end;
+
+    Assign(output, 'z3.out.txt');
+    Rewrite(output);
+    WriteLn(sum);
+    Close(output);
+end.
